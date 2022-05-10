375451d0f46f2119d45fffd0b3f814042f6ae01d
extensaman <verus.wedding@gmail.com>
2022-05-10 23:17:25 +0300

    Don't know

--- src/app/views/coach/coach.component.ts
+++ src/app/views/coach/coach.component.ts
@@ -2,10 +2,13 @@ import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import {Coach} from "../../model/Coach";
 import {DataHandlerService} from "../../service/data-handler.service";
 import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
+import {MatSort, SortDirection} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import {CoachDialogComponent} from "./coach-dialog/coach-dialog.component";
+import {merge, Observable, of as observableOf} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-coach',
@@ -16,14 +19,20 @@ export class CoachComponent implements OnInit, AfterViewInit {
 
   coaches: Coach[] = [];
 
-  displayedColumns: string[] = ['id', 'surname', 'name', 'patronymic', 'horseCount'];
-  dataSource: MatTableDataSource<Coach>;
+  displayedColumns2: string[] = ['id', 'surname', 'name', 'patronymic', 'horseCount'];
+  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
+  exampleDatabase: ExampleHttpDatabase | null;
+  data: GithubIssue[] = [];
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
   @ViewChild(MatSort) sort: MatSort | undefined;
 
-  constructor(private dataHandler: DataHandlerService, private dialog: MatDialog) {
-    this.dataSource = new MatTableDataSource<Coach>(this.coaches);
+  constructor(private dataHandler: DataHandlerService, private dialog: MatDialog,
+              private _httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
@@ -31,23 +40,78 @@ export class CoachComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    // @ts-ignore
-    this.dataSource.paginator = this.paginator;
-    // @ts-ignore
-    this.dataSource.sort = this.sort;
+    this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
+
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.exampleDatabase!.getRepoIssues(
+            this.sort.active,
+            this.sort.direction,
+            this.paginator.pageIndex,
+          ).pipe(catchError(() => observableOf(null)));
+        }),
+        map(data => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = data === null;
+
+          if (data === null) {
+            return [];
+          }
+
+          // Only refresh the result length if there is new data. In case of rate
+          // limit errors, we do not want to reset the paginator to zero, as that
+          // would prevent users from re-triggering requests.
+          this.resultsLength = data.total_count;
+          return data.items;
+        }),
+      )
+      .subscribe(data => (this.data = data));
   }
 
-  applyFilter(event: Event) {
+/*  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-  }
+  }*/
 
   openDialog(coach: Coach) {
     this.dataHandler.setSelectedCoach(coach);
     this.dialog.open(CoachDialogComponent);
+  }
+}
+
+export interface GithubApi {
+  items: GithubIssue[];
+  total_count: number;
+}
+
+export interface GithubIssue {
+  created_at: string;
+  number: string;
+  state: string;
+  title: string;
+}
+
+/** An example database that the data source uses to retrieve data for the table. */
+export class ExampleHttpDatabase {
+  constructor(private _httpClient: HttpClient) {}
+
+  getRepoIssues(sort: string, order: SortDirection, page: number): Observable<GithubApi> {
+    const href = 'https://api.github.com/search/issues';
+    const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
+      page + 1
+    }`;
+
+    return this._httpClient.get<GithubApi>(requestUrl);
   }
 }
